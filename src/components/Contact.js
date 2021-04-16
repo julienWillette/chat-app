aef15b068c1051494efbe0662f0335932eca8158
julien willette <julien.willette@gmail.com>
2021-04-16 13:39:18 +0200

    add state for Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,32 +5,47 @@ function online(online) {
     if (online) {
       return (
         <div className="status">
-          <div className="status-online"></div>
           <div className="status-text">Online</div>
         </div>
       );
     } else {
       return (
         <div className="status">
-          <div className="status-offline"></div>
           <p className="status-text">Offline</p>
         </div>
       );
     }
   }
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
+  render() {
     return (
           <div className="Contact">
-            <img className="avatar" src={props.image} alt={props.name}/>
+            <img className="avatar" src={this.props.image} alt={this.props.name}/>
             <div>
-              <h4 className="name">{props.name}</h4>
+              <h4 className="name">{this.props.name}</h4>
               <div className="status">
-                    {online(props.online)}
+                      <button className={this.state.online ? 'status-online' : 'status-offline'}
+                      onClick={event => {
+                      const newOnline = !this.state.online;
+                      this.setState({ online: newOnline });
+              }}
+              >
+                <div className="status-text">
+                {online(this.props.online)}
+                </div>
+              </button>
             </div>
           </div>
           </div>
         );
+  }
 };
   
   export default Contact;
