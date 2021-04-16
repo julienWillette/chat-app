c68fcca48f989da1957d77756e78a9fa2f3ce1c2
julien willette <julien.willette@gmail.com>
2021-04-16 09:57:19 +0200

    add alt for image in Conatct.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,7 @@ function online(online) {
 function Contact(props) {
     return (
           <div className="Contact">
-            <img className="avatar" src={props.image}  />
+            <img className="avatar" src={props.image} alt={props.name}/>
             <div>
               <h4 className="name">{props.name}</h4>
               <div className="status">
